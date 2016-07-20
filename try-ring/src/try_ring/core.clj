94db5aa36f3cfa731a1787d8ce9eba1ed4c12a6b
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2016-07-20 21:35:57 +0400

    add ring

--- try-ring/src/try_ring/core.clj
+++ try-ring/src/try_ring/core.clj
@@ -1,7 +1,12 @@
 (ns try-ring.core
+  (:require [ring.adapter.jetty :refer [run-jetty]])
   (:gen-class))
 
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (run-jetty handler {:port 3001}))
